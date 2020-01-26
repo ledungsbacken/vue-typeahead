8c1baf17bfc29775c8a3a6625c76b18c83e21946
ledungsbacken <ledungsbacken@gmail.com>
2020-01-26 04:28:46 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <typeahead
+        v-model="selectedId"
+        :data="data"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Typeahead from './components/Typeahead.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        Typeahead
+    },
+    data() {
+        return {
+            selectedId: null,
+            data: [
+                {
+                    id: 1,
+                    label: 'thing1',
+                },
+                {
+                    id: 2,
+                    label: 'something2',
+                },
+                {
+                    id: 3,
+                    label: 'thingsome3',
+                },
+                {
+                    id: 4,
+                    label: 'some4',
+                },
+            ]
+        };
+    },
+    // computed: {
+    //     selectedLabel() {
+    //         if (!this.selectedId) {
+    //             return '';
+    //         }
+    //         return this.data.find(e => e.id === this.selectedId).label;
+    //     },
+    // },
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
